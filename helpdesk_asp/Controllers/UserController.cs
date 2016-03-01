6d0cd8bd487b39a07d4bef656dafaf8779b78349
andree <hackartz@gmail.com>
2016-03-01 19:43:58 +0700

    add code

---
+++ helpdesk_asp/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using model = helpdesk_asp.Models;
+using DapperLib;
+
+namespace helpdesk_asp.Controllers
+{
+    [RoutePrefix("User")]
+    public class UserController : Controller
+    {
+        //TODO: 3 form order (computer,software, dan telp)
+
+        private SQLConn db = new SQLConn(System.Configuration.ConfigurationManager.AppSettings["strCnn"].ToString());
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("tes")]
+        public ActionResult Tes()
+        {
+            var result = this.db.QuerySPtoIEnumerable<model.KonsulBagian>("Sp_GetKonsulBagian");
+            return Json(result,JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ helpdesk_asp/Models/HelpDeskModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace helpdesk_asp.Models
+{
+    public class KonsulBagian
+    {
+        public string Bagian { get; set; }
+        public string Telepon { get; set; }
+    }
+}
